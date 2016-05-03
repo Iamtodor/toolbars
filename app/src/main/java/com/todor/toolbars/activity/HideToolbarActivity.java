e8d3928952a201122a3a81829e210e9858a2dc7b
Iamtodor <todor.ilya@gmail.com>
2016-05-03 21:03:04 +0300

    Added fab

--- app/src/main/java/com/todor/toolbars/activity/HideToolbarActivity.java
+++ app/src/main/java/com/todor/toolbars/activity/HideToolbarActivity.java
@@ -1,6 +1,7 @@
 package com.todor.toolbars.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
@@ -12,6 +13,7 @@ public class HideToolbarActivity extends BaseActivity {
 
     @BindView(R.id.toolbar) protected Toolbar toolbar;
     @BindView(R.id.recycler_view) protected RecyclerView recyclerView;
+    @BindView(R.id.fab) protected FloatingActionButton fab;
 
     @Override
     public int getContentViewId() {
@@ -24,6 +26,18 @@ public class HideToolbarActivity extends BaseActivity {
 
         setupRecyclerView(recyclerView);
         setupToolbar(toolbar);
+        setFabBehavior();
     }
 
+    private void setFabBehavior() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0 && fab.isShown())
+                    fab.hide();
+                else if (dy < 0 && !fab.isShown())
+                    fab.show();
+            }
+        });
+    }
 }
