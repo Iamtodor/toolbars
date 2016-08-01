60d1106eabcde4e2df21416cd48eb662d6b4b0d9
iamtodor <todor.ilya@gmail.com>
2016-08-01 19:57:17 +0300

    Added big text and big picture notifications

--- app/src/main/java/com/todor/toolbars/activity/NotificationsActivity.java
+++ app/src/main/java/com/todor/toolbars/activity/NotificationsActivity.java
@@ -31,26 +31,51 @@ public class NotificationsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupToolbar(mToolbar);
+
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mLargeItem = BitmapFactory.decodeResource(getResources(), R.drawable.screenshot);
     }
 
     @OnClick(R.id.simple_notification)
     public void simpleNotification() {
         mLargeItem = BitmapFactory.decodeResource(getResources(), R.drawable.screenshot);
-        if(Build.VERSION.SDK_INT >= 21) {
-            mNotification = new Notification.Builder(this)
-                    .setSmallIcon(R.drawable.notification_small_icon)
-                    .setLargeIcon(mLargeItem)
-                    .setContentTitle(getString(R.string.notification_title))
-                    .setContentText(getString(R.string.notification_content))
-                    .setColor(ContextCompat.getColor(this, android.R.color.holo_red_light));
-        } else {
-            mNotification = new Notification.Builder(this)
-                    .setSmallIcon(R.drawable.notification_small_icon)
-                    .setLargeIcon(mLargeItem)
-                    .setContentTitle(getString(R.string.notification_title))
-                    .setContentText(getString(R.string.notification_content));
+        mNotification = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.notification_small_icon)
+                .setLargeIcon(mLargeItem)
+                .setContentTitle("Simple notification")
+                .setContentText(getString(R.string.notification_content));
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            mNotification.setColor(ContextCompat.getColor(this, android.R.color.holo_red_light));
         }
-        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
         mNotificationManager.notify(1, mNotification.build());
+    }
+
+    @OnClick(R.id.big_text_notification)
+    public void bigTextNotification() {
+        mNotification = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.notification_small_icon)
+                .setLargeIcon(mLargeItem)
+                .setContentTitle("Big text notification")
+                .setStyle(new Notification.BigTextStyle().bigText(getString(R.string.long_string)));
+
+        mNotificationManager.notify(2, mNotification.build());
+    }
+
+    @OnClick(R.id.big_picture_notification)
+    public void bigPictureNotification() {
+        mNotification = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.notification_small_icon)
+                .setContentTitle("Big text notification")
+                .setContentText("Content text")
+                .setSubText("Sub text")
+                .setStyle(new Notification.BigPictureStyle().bigPicture(mLargeItem));
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            mNotification.setColor(ContextCompat.getColor(this, android.R.color.holo_red_light));
+        }
+
+        mNotificationManager.notify(3, mNotification.build());
     }
 }
